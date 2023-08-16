98f4a4d1b6a4225aa8978e60a93c34d04789370d
Aleksandr Shershnev <shershnev.av@gmail.com>
2023-08-16 18:20:14 +0300

    fixed routing

---
+++ src/feed/ui/feed-managing-dialog-folders-item-menu.tsx
@@ -0,0 +1,85 @@
+import type { Component } from 'solid-js'
+import { For, createSignal, createMemo, onCleanup } from 'solid-js'
+import { clsx } from 'clsx'
+
+import { localeState } from '~/core/locale'
+import { routingState, popRoute } from '~/shared/routing'
+import type { IconProps } from '~/shared/ui/elements'
+import { Button, Icon, Text } from '~/shared/ui/elements'
+
+import type { Folder } from '../feed.types'
+import { feedRoutes } from '../feed-routes'
+
+import * as feedManagingDialogFoldersItemMenuCSS from './feed-managing-dialog-folders-item-menu.sss'
+
+export type FeedManagingDialogFoldersItemMenuProps = {
+  onEdit: FolderActionHandler
+  onMoveUp?: FolderIndexActionHandler
+  onMoveDown?: FolderIndexActionHandler
+  onDelete?: FolderActionHandler
+  onConfirm?: FolderActionHandler
+}
+
+type FolderMenuItem = {
+  icon: IconProps['name']
+  text?: string
+  warning?: boolean
+  onClick?: () => void
+}
+
+type FolderActionHandler = [
+  (folder: Folder, ev) => void,
+  Folder
+]
+
+type FolderIndex = {
+  index: number
+  k: number
+}
+
+type FolderIndexActionHandler = [
+  (folderIndex: FolderIndex, ev) => void,
+  FolderIndex
+]
+
+export const FeedManagingDialogFoldersItemMenu: Component<FeedManagingDialogFoldersItemMenuProps> = (props) => {
+  const [isDeleting, setDeleting] = createSignal(false)
+
+  const startDeleting = () => setDeleting(true)
+
+  const getMenuItems = createMemo<FolderMenuItem[]>(() => [
+    { icon: 'edit', text: localeState.texts?.feed.buttons.edit, onClick: props.onEdit },
+    props.onMoveUp && { icon: 'arrowUpward', text: localeState.texts?.feed.buttons.moveUp, onClick: props.onMoveUp },
+    props.onMoveDown && { icon: 'arrowDownward', text: localeState.texts?.feed.buttons.moveDown, onClick: props.onMoveDown },
+    !isDeleting() && { icon: 'delete', text: localeState.texts?.feed.buttons.delete, onClick: startDeleting },
+    isDeleting() && { icon: 'delete', text: localeState.texts?.feed.buttons.confirm, onClick: props.onDelete, warning: true },
+  ].filter(item => !!item) as FolderMenuItem[])
+
+  onCleanup(() => {
+    const folderMenuRoute = routingState.history.findLast(route =>
+      route.id === feedRoutes.managingDialogFolderMenu.id
+    )
+    if (!folderMenuRoute) return
+    popRoute(feedRoutes.managingDialogFolderMenu)
+  })
+
+  return (
+    <div class={feedManagingDialogFoldersItemMenuCSS.base}>
+      <For each={getMenuItems()}>{item => (
+        <Button
+          class={clsx(
+            feedManagingDialogFoldersItemMenuCSS.button,
+            item.warning && feedManagingDialogFoldersItemMenuCSS._warning
+          )}
+          onClick={item.onClick}
+          stopPropagation
+        >
+          <Icon name={item.icon} size='small'/>
+          <Text variant='label' size='large' ellipsis>
+            {item.text}
+          </Text>
+        </Button>
+      )}</For>
+    </div>
+  )
+}
--- src/feed/ui/feed-managing-dialog-folders-item.tsx
+++ src/feed/ui/feed-managing-dialog-folders-item.tsx
@@ -1,50 +1,23 @@
 import type { Component,  } from 'solid-js'
-import { Show, For, createSignal, createMemo, createEffect, batch } from 'solid-js'
+import { Show, splitProps, createSignal, createMemo, createEffect } from 'solid-js'
 import type { TransitionProps } from 'solid-transition-group'
 import { Transition } from 'solid-transition-group'
 import { clsx } from 'clsx'
 
-import { localeState } from '~/core/locale'
-import { routingState, pushRoute, popRoute } from '~/shared/routing'
-import type { IconProps } from '~/shared/ui/elements'
+import { routingState, pushRoute } from '~/shared/routing'
 import { Button, Icon, Text } from '~/shared/ui/elements'
 import { getScaleInParams, getScaleOutParams } from '~/shared/ui/animations'
 
 import type { Folder } from '../feed.types'
 import { feedRoutes } from '../feed-routes'
+import type { FeedManagingDialogFoldersItemMenuProps } from './feed-managing-dialog-folders-item-menu'
+import { FeedManagingDialogFoldersItemMenu } from './feed-managing-dialog-folders-item-menu'
+
 import * as layoutCSS from '../../shared/ui/elements/layout.sss'
 import * as feedManagingDialogFoldersItemCSS from './feed-managing-dialog-folders-item.sss'
 
-export type FeedManagingDialogFoldersItemProps = {
+export type FeedManagingDialogFoldersItemProps = FeedManagingDialogFoldersItemMenuProps & {
   folder: Folder
-  movable?: boolean
-  onEdit: FolderActionHandler
-  onMoveUp?: FolderIndexActionHandler
-  onMoveDown?: FolderIndexActionHandler
-  onDelete?: FolderActionHandler
-  onConfirm?: FolderActionHandler
-}
-
-type FolderActionHandler = [
-  (folder: Folder, ev) => void,
-  Folder
-]
-
-type FolderIndex = {
-  index: number
-  k: number
-}
-
-type FolderIndexActionHandler = [
-  (folderIndex: FolderIndex, ev) => void,
-  FolderIndex
-]
-
-type FolderMenuItem = {
-  icon: IconProps['name']
-  text?: string
-  warning?: boolean
-  onClick?: () => void
 }
 
 const ANIMATION_PARAMS = {
@@ -52,9 +25,10 @@ const ANIMATION_PARAMS = {
   exit: getScaleOutParams({ scale: '1 0' })
 }
 
-export const FeedManagingDialogFoldersItem: Component<FeedManagingDialogFoldersItemProps> = (props) => {
+export const FeedManagingDialogFoldersItem: Component<FeedManagingDialogFoldersItemProps> = (_props) => {
+  const [props, menuProps] = splitProps(_props, ['folder'])
+
   const [isExpanded, setExpanded] = createSignal(false)
-  const [isDeleting, setDeleting] = createSignal(false)
 
   const isFolderMenuRoute = createMemo(() => {
     return routingState.currentRoute.id === feedRoutes.managingDialogFolderMenu.id
@@ -66,18 +40,12 @@ export const FeedManagingDialogFoldersItem: Component<FeedManagingDialogFoldersI
   }
 
   const closeFolderMenu = () => {
-    popRoute(feedRoutes.managingDialogFolderMenu)
-    batch(() => {
-      setExpanded(false)
-      setDeleting(false)
-    })
+    setExpanded(false)
   }
 
   const toggleExpanding = () => {
     isExpanded() ? closeFolderMenu() : openFolderMenu()
   }
-
-  const toggleDeleting = () => setDeleting(true)
 
   createEffect(() => {
     if (!isFolderMenuRoute() && isExpanded()) {
@@ -85,22 +53,14 @@ export const FeedManagingDialogFoldersItem: Component<FeedManagingDialogFoldersI
     }
   })
 
-  const getMenuItems = createMemo<FolderMenuItem[]>(() => [
-    { icon: 'edit', text: localeState.texts?.feed.buttons.edit, onClick: props.onEdit },
-    props.onMoveUp && { icon: 'arrowUpward', text: localeState.texts?.feed.buttons.moveUp, onClick: props.onMoveUp },
-    props.onMoveDown && { icon: 'arrowDownward', text: localeState.texts?.feed.buttons.moveDown, onClick: props.onMoveDown },
-    !isDeleting() && { icon: 'delete', text: localeState.texts?.feed.buttons.delete, onClick: toggleDeleting },
-    isDeleting() && { icon: 'delete', text: localeState.texts?.feed.buttons.confirm, onClick: props.onDelete, warning: true },
-  ].filter(item => !!item) as FolderMenuItem[])
-
-  const handleEnter: TransitionProps['onEnter'] = async (el, done) => {
+  const handleMenuEnter: TransitionProps['onEnter'] = async (el, done) => {
     const animationParams = ANIMATION_PARAMS.enter
     const animation = el.animate(animationParams.keyframes, animationParams.options)
     await animation.finished
     done()
   }
 
-  const handleExit: TransitionProps['onExit'] = async (el, done) => {
+  const handleMenuExit: TransitionProps['onExit'] = async (el, done) => {
     const animationParams = ANIMATION_PARAMS.exit
     const animation = el.animate(animationParams.keyframes, animationParams.options)
     await animation.finished
@@ -126,27 +86,13 @@ export const FeedManagingDialogFoldersItem: Component<FeedManagingDialogFoldersI
       </Button>
 
       <Transition
-        onEnter={handleEnter}
-        onExit={handleExit}
+        onEnter={handleMenuEnter}
+        onExit={handleMenuExit}
       >
         <Show when={isExpanded()}>
-          <div class={feedManagingDialogFoldersItemCSS.menu}>
-            <For each={getMenuItems()}>{item => (
-              <Button
-                class={clsx(
-                  feedManagingDialogFoldersItemCSS.button,
-                  item.warning && feedManagingDialogFoldersItemCSS._warning
-                )}
-                onClick={item.onClick}
-                stopPropagation
-              >
-                <Icon name={item.icon} size='small'/>
-                <Text variant='label' size='large' ellipsis>
-                  {item.text}
-                </Text>
-              </Button>
-            )}</For>
-          </div>
+          <FeedManagingDialogFoldersItemMenu
+            {...menuProps}
+          />
         </Show>
       </Transition>
     </div>
