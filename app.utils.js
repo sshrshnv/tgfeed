0ef5be11e104d9ff10d4bc610be44b7051cb6800
Aleksandr Shershnev <shershnev.av@gmail.com>
2023-10-08 20:02:50 +0300

    icons

--- app.utils.js
+++ app.utils.js
@@ -10,6 +10,25 @@ const getLocaleLangs = () => {
   }).reverse()
 }
 
+const generateIOSSplashScreens = () => {
+  const path = require('path')
+  const pwaAssetGenerator = require('pwa-asset-generator')
+  const appConfig = require('./app.config')
+
+  pwaAssetGenerator.generateImages(
+    path.resolve('./src/shared/ui/icons/manifest/maskable.svg'),
+    path.resolve('./src/shared/ui/icons/manifest/splash'),
+    {
+      index: path.resolve('./src/app.html'),
+      background: appConfig.APP_DEFAULT_THEME_COLOR,
+      padding: `calc(50vh - 48px) calc(50vw - 48px)`,
+      splashOnly: true,
+      type: 'png'
+    }
+  )
+}
+
 module.exports = {
-  getLocaleLangs
+  getLocaleLangs,
+  generateIOSSplashScreens
 }
